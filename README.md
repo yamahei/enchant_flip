09e822070a1c62566676c7468c4558106fe83cdc
yamaorii <yamaorii@gmail.com>
2021-05-04 21:45:33 +0900

    PWA対応（いけたかも

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Flip!
 読み込みの都合があり、Webサーバを起動します
 
 ```sh
+cd ..
 ruby -rsinatra -e 'set :bind, "0.0.0.0"; set :public_folder, "./", get("/"){"Hello world"}'
 ```
--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,11 +1,174 @@
+
+/**
+ * キャッシュコントロール
+ * CACHE_STORAGE_NAME: キャッシュ名
+ *     バージョンアップごとにカウントアップ（再キャッシュ用）
+ * files_to_cache: 対象ファイルリスト
+ *     ここにないとキャッシュされない
+ *     パス「../enchant_moriy」にて
+ *     以下のコマンドを実行する
+ *     $ find . -type f | grep "enchant_flip" | grep -v ".git" | sed 's/^\.//'
+ */
+const CACHE_STORAGE_NAME = 'v1';
+const files_to_cache = `
+/enchant_flip/apad.png
+/enchant_flip/bar.png
+/enchant_flip/bgManager.js
+/enchant_flip/cardManager.js
+/enchant_flip/charManager.js
+/enchant_flip/controller.js
+/enchant_flip/enchant-plugins/ui.enchant.js
+/enchant_flip/enchant-plugins/_memory.enchant.js
+/enchant_flip/enchant-plugins/_nineleap.enchant.js
+/enchant_flip/enchant-plugins/_twitter.enchant.js
+/enchant_flip/enchant.js
+/enchant_flip/enchant.min.js
+/enchant_flip/font0.png
+/enchant_flip/game.js
+/enchant_flip/icon0.png
+/enchant_flip/index.html
+/enchant_flip/infoManager.js
+/enchant_flip/LICENSE
+/enchant_flip/manifest.webmanifest
+/enchant_flip/object.js
+/enchant_flip/pad.png
+/enchant_flip/promote/commercial_image1024x500.png
+/enchant_flip/promote/commercial_image1024x500.xcf
+/enchant_flip/promote/editing/bg/bg_dirt-dent.png
+/enchant_flip/promote/editing/bg/bg_dirt-gravel.png
+/enchant_flip/promote/editing/bg/bg_dirt-ledge.png
+/enchant_flip/promote/editing/bg/bg_dirt-moss.png
+/enchant_flip/promote/editing/bg/bg_glass-fence.png
+/enchant_flip/promote/editing/bg/bg_glass-ground.png
+/enchant_flip/promote/editing/bg/bg_glass-tile.png
+/enchant_flip/promote/editing/bg/bg_land-gravel.png
+/enchant_flip/promote/editing/bg/bg_land-weed.png
+/enchant_flip/promote/editing/bg/bg_lava-dirt.png
+/enchant_flip/promote/editing/bg/bg_lava-ore.png
+/enchant_flip/promote/editing/bg/bg_ore-gravel.png
+/enchant_flip/promote/editing/bg/bg_steppe-cobbled.png
+/enchant_flip/promote/editing/bg/bg_tile-dent.png
+/enchant_flip/promote/editing/bg/bg_tile-ledge.png
+/enchant_flip/promote/editing/bg/bg_tile-water.png
+/enchant_flip/promote/editing/bg/bg_weed-gravel.png
+/enchant_flip/promote/editing/char/card.xcf
+/enchant_flip/promote/editing/char/effect.xcf
+/enchant_flip/promote/editing/se/aha_voice015.wav
+/enchant_flip/promote/editing/se/a_bingo_coin03.wav
+/enchant_flip/promote/editing/se/a_clear_animal01.wav
+/enchant_flip/promote/editing/se/a_flip_paper00.wav
+/enchant_flip/promote/editing/se/a_frame_fire01.wav
+/enchant_flip/promote/editing/se/a_gameover_animal02.wav
+/enchant_flip/promote/editing/se/a_hurry_beep12.wav
+/enchant_flip/promote/editing/se/a_icing_freeze04.wav
+/enchant_flip/promote/editing/se/a_memorize_whistle00.wav
+/enchant_flip/promote/editing/se/a_move_step04.wav
+/enchant_flip/promote/editing/se/a_NG_beep14.wav
+/enchant_flip/promote/editing/se/a_poison_fm008.wav
+/enchant_flip/promote/editing/se/a_start_whistle02.wav
+/enchant_flip/promote/editing/se/bingo.mp3
+/enchant_flip/promote/editing/se/bingo.wav
+/enchant_flip/promote/editing/se/bingo_cursor15_d.wav
+/enchant_flip/promote/editing/se/clear.wav
+/enchant_flip/promote/editing/se/clear_m.wav
+/enchant_flip/promote/editing/se/flip_bosu39.wav
+/enchant_flip/promote/editing/se/flip_clock01.wav
+/enchant_flip/promote/editing/se/flip_mizu07.wav
+/enchant_flip/promote/editing/se/framing.mp3
+/enchant_flip/promote/editing/se/framing.wav
+/enchant_flip/promote/editing/se/gameover.wav
+/enchant_flip/promote/editing/se/hurry_pyoro00.wav
+/enchant_flip/promote/editing/se/icing3.mp3
+/enchant_flip/promote/editing/se/icing3.wav
+/enchant_flip/promote/editing/se/move.mp3
+/enchant_flip/promote/editing/se/move.wav
+/enchant_flip/promote/editing/se/NG.mp3
+/enchant_flip/promote/editing/se/NG.wav
+/enchant_flip/promote/editing/se/NG_bell06.wav
+/enchant_flip/promote/editing/se/NG_byoro03.wav
+/enchant_flip/promote/editing/se/NG_cursor00_c.wav
+/enchant_flip/promote/editing/se/poisenate3.mp3
+/enchant_flip/promote/editing/se/poisenate3.wav
+/enchant_flip/promote/editing/se/sec_beep08.wav
+/enchant_flip/promote/iconimage36x36.png
+/enchant_flip/promote/iconimage48x48.png
+/enchant_flip/promote/iconimage512x512.png
+/enchant_flip/promote/iconimage72x72.png
+/enchant_flip/promote/iconimage_org.xcf
+/enchant_flip/promote/promotion180x120.png
+/enchant_flip/promote/screenshot/deal.png
+/enchant_flip/promote/screenshot/helper.png
+/enchant_flip/promote/screenshot/memo.png
+/enchant_flip/promote/screenshot/memo_trim.png
+/enchant_flip/promote/screenshot/switch.png
+/enchant_flip/promote/screenshot/title.png
+/enchant_flip/pwa.html
+/enchant_flip/pwabuilder-sw.js
+/enchant_flip/README.md
+/enchant_flip/resource/aha_voice015.mp3
+/enchant_flip/resource/aha_voice015.wav
+/enchant_flip/resource/bg_dirt-gravel.png
+/enchant_flip/resource/bg_dirt-ledge.png
+/enchant_flip/resource/bg_glass-ground.png
+/enchant_flip/resource/bg_glass-tile.png
+/enchant_flip/resource/bg_land-gravel.png
+/enchant_flip/resource/bg_land-weed.png
+/enchant_flip/resource/bg_lava-ore.png
+/enchant_flip/resource/bg_steppe-cobbled.png
+/enchant_flip/resource/bingo_coin03.mp3
+/enchant_flip/resource/bingo_coin03.wav
+/enchant_flip/resource/card.gif
+/enchant_flip/resource/char_dwarf.gif
+/enchant_flip/resource/char_fairy.gif
+/enchant_flip/resource/char_frame.gif
+/enchant_flip/resource/char_girl.gif
+/enchant_flip/resource/char_magician.gif
+/enchant_flip/resource/char_snow.gif
+/enchant_flip/resource/clear_animal01.mp3
+/enchant_flip/resource/clear_animal01.wav
+/enchant_flip/resource/effect.gif
+/enchant_flip/resource/flip_sha00.mp3
+/enchant_flip/resource/flip_sha00.wav
+/enchant_flip/resource/frame_fire01.mp3
+/enchant_flip/resource/frame_fire01.wav
+/enchant_flip/resource/gameover_animal02.mp3
+/enchant_flip/resource/gameover_animal02.wav
+/enchant_flip/resource/hurry_beep12.mp3
+/enchant_flip/resource/hurry_beep12.wav
+/enchant_flip/resource/icing_freeze04.mp3
+/enchant_flip/resource/icing_freeze04.wav
+/enchant_flip/resource/memorize_whistle00.mp3
+/enchant_flip/resource/memorize_whistle00.wav
+/enchant_flip/resource/move_step04_m.mp3
+/enchant_flip/resource/move_step04_m.wav
+/enchant_flip/resource/NG_beep14.mp3
+/enchant_flip/resource/NG_beep14.wav
+/enchant_flip/resource/poison_fm008.mp3
+/enchant_flip/resource/poison_fm008.wav
+/enchant_flip/resource/start_whistle02.mp3
+/enchant_flip/resource/start_whistle02.wav
+/enchant_flip/resource/title.gif
+/enchant_flip/stageManager.js
+/enchant_flip/strage.js
+/enchant_flip/titleManager.js
+`.split("\n")
+.map(function(line){
+  return line.replace(/^\s+|\s+$/g, "");
+})
+.filter(function(line){
+  return !!line;
+});
+
+
+
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
+// const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+// const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -15,8 +178,9 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE_STORAGE_NAME)
+      .then((cache) => cache.addAll(files_to_cache))
+      // .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
@@ -38,8 +202,8 @@ self.addEventListener('fetch', (event) => {
         return networkResp;
       } catch (error) {
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
+        const cache = await caches.open(CACHE_STORAGE_NAME);
+        const cachedResp = await cache.match(files_to_cache);
         return cachedResp;
       }
     })());
